a5382f3864f2d4f11c716f850fe3760fba30afc3
DongHyunKims <eldk1229@gmail.com>
2018-05-31 02:06:36 +0900

    Encoding 공부중

---
+++ encodeing/convert.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const iconv = require('iconv-lite');
+const buf = fs.readFileSync(`${__dirname}/resources/euckr.txt`);
+
+const utf8 = buf.toString();
+console.log('UTF8:', utf8);
+
+const euckr = iconv.decode(buf, 'euckr');
+console.log('EUCKR:', euckr);
+
+// 쓸 때
+console.log('----------------------------');
+
+const data = iconv.encode('가나다', 'euckr');
+console.log(data);
+
+fs.writeFileSync('_euckr.txt', data, {
+    encoding: null
+});
+
+console.log(iconv.decode(fs.readFileSync('_euckr.txt'), 'euckr'));
